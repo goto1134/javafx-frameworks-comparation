fabf6db5f4e3a1e0bccf0f157b7d269dc63d3693
Andrew Yefanov <1134togo@gmail.com>
2017-08-13 13:54:57 +0300

    Add afterburner view

---
+++ afterburnerfx/src/main/kotlin/com/github/goto1134/fxframeworks/AfterburnerPresenter.kt
@@ -0,0 +1,22 @@
+package com.github.goto1134.fxframeworks
+
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+
+/**
+ * Created by Andrew
+ * on 13.08.2017.
+ */
+class AfterburnerPresenter {
+
+    @FXML
+    val nameTextField: TextField? = null
+    @FXML
+    val surnameTextField: TextField? = null
+    @FXML
+    val helloLabel: Label? = null
+    @FXML
+    val sayHelloButton: Button? = null
+}
---
+++ afterburnerfx/src/main/kotlin/com/github/goto1134/fxframeworks/AfterburnerView.kt
@@ -0,0 +1,10 @@
+package com.github.goto1134.fxframeworks
+
+import com.airhacks.afterburner.views.FXMLView
+
+/**
+ * Created by Andrew
+ * on 13.08.2017.
+ */
+class AfterburnerView: FXMLView() {
+}
