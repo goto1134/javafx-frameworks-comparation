503d64d2f07ef811829ecb98ea13f48873b93668
Andrew Yefanov <1134togo@gmail.com>
2017-08-13 14:10:45 +0300

    Add service injection in presenter

--- afterburnerfx/src/main/kotlin/com/github/goto1134/fxframeworks/AfterburnerPresenter.kt
+++ afterburnerfx/src/main/kotlin/com/github/goto1134/fxframeworks/AfterburnerPresenter.kt
@@ -1,22 +1,27 @@
 package com.github.goto1134.fxframeworks
 
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
+import java.net.URL
+import java.util.*
+import javax.inject.Inject
 
 /**
  * Created by Andrew
  * on 13.08.2017.
  */
-class AfterburnerPresenter {
+class AfterburnerPresenter() : Initializable {
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        sayHelloButton.setOnAction { helloLabel.text = service.getHelloStringForUser(User(nameTextField.text, surnameTextField.text)) }
+    }
 
-    @FXML
-    val nameTextField: TextField? = null
-    @FXML
-    val surnameTextField: TextField? = null
-    @FXML
-    val helloLabel: Label? = null
-    @FXML
-    val sayHelloButton: Button? = null
+
+    @Inject lateinit var service: HelloUserServiceImpl
+    @FXML lateinit var nameTextField: TextField
+    @FXML lateinit var surnameTextField: TextField
+    @FXML lateinit var helloLabel: Label
+    @FXML lateinit var sayHelloButton: Button
 }
